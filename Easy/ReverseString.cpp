b0ef10bd7faa8025349fdcd70b36ca37720e21c4
Gadaguer Leon <gadaguerleon@gmail.com>
2021-11-11 14:25:55 -0600

    Added algorithm and driver code

--- Easy/ReverseString.cpp
+++ Easy/ReverseString.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+void reverseString(std::string& s) {
+    int left = 0; 
+    int right = s.size() - 1;
+    while (left < right) {
+        std::swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+    for (int i = 0; i < s.size(); i++) {
+        std::cout << s[i];
+    }
+}
+
 int main() {
-    
+    std::string test = "Stevie Ray Vaughan";
+    reverseString(test);
 }
