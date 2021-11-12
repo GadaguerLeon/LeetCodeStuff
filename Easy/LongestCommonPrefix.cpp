f25ed8a22a320712dd191c3946aa1d042e432a3c
Gadaguer Leon <gadaguerleon@gmail.com>
2021-11-12 11:35:44 -0600

    Updated problem explanation and driver code

--- Easy/LongestCommonPrefix.cpp
+++ Easy/LongestCommonPrefix.cpp
@@ -3,7 +3,18 @@
 #include <algorithm>
 /*Write a function to find the longest common prefix string amongst an array of strings.
 
-If there is no common prefix, return an empty string "".*/
+If there is no common prefix, return an empty string "".
+
+Example 1
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+*/
 
 std::string longestCommonPrefix(std::vector<std::string> strs) {
     std::string ans;
@@ -22,7 +33,9 @@ std::string longestCommonPrefix(std::vector<std::string> strs) {
 
 // Driver code
 int main () {
-    std::vector<std::string> test = {"flower","flow","flight"};
-    std::cout << longestCommonPrefix(test) << std::endl;
+    std::vector<std::string> one = {"flower","flow","flight"};
+    std::vector<std::string> two = {"dog","racecar","car"};
+    std::cout << longestCommonPrefix(one) << std::endl;
+    std::cout << longestCommonPrefix(two) << std::endl;
     return 0;
 }
