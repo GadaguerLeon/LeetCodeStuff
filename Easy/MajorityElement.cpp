3b0f51fad85796e4e28a5aed215ad48032817243
Gadaguer Leon <gadaguerleon@gmail.com>
2021-11-24 15:30:00 -0600

    Added solution for Majority Element problem

---
+++ Easy/MajorityElement.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+*/
+int majorityElement(std::vector<int>& nums) {
+    int cand = nums[0];
+    int count = 1;
+    for (int i = 1; i < nums.size(); i++) {
+        if (cand == nums[i]) {
+            count++;
+        } else {
+            count--;
+            if (count == 0) {
+                cand = nums[i];
+                count = 1;
+            }
+        }
+    }
+    return cand;
+}
+
+// Driver code
+int main () {
+    std::vector<int> one = {3, 2, 3};
+    std::vector<int> two = {2, 2, 1, 1, 1, 2, 2};
+    
+    std::cout << majorityElement(one) << std::endl;
+    std::cout << majorityElement(two) << std::endl;
+    return 0;
+}
